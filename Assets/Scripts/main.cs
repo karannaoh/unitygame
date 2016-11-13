9ed79b612b05e85784f7127fd82b8c5aad8a6b23
karannaoh <karanpratapsingh43@gmail.com>
2016-11-13 10:41:55 +0530

    restart level and next level

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class main : MonoBehaviour {
     public float thrust_force = 5f;
     public float velocityx = 0;
     public float velocityy = 0;
     public float fuel = 500.0f;
-
+    public GameObject button;
+    public GameObject target;
+ 
     // Use this for initialization
-    void Start () {
-        GameObject[] planets;
-        planets = GameObject.FindGameObjectsWithTag("gravity");
-    }
+   
+         
+         void Start () {
+
+        button.SetActive(false);
+
+           }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,12 +30,21 @@ public class main : MonoBehaviour {
         velocityx = velocityx + net_forceX * Time.deltaTime;
         velocityy = velocityy + net_forceY * Time.deltaTime;
         transform.Translate(velocityx, 0 , velocityy, Space.World);
+        if (getDistance(target.transform.position, this.transform.position)<2)
+           {
+           int scene1 = SceneManager.GetActiveScene().buildIndex;
+            if (scene1 < 3)
+            {
+             scene1++; }
+            SceneManager.LoadScene(scene1);    
+           }
     }
 
     public float thrust_value(int x)
     {
         if(fuel <= 0)
         {
+            button.SetActive(true);
             return 0.0f;
         }
         float forceX = 0;
@@ -49,7 +66,7 @@ public class main : MonoBehaviour {
         {
             forceX = -thrust_force;
         }
-        print(fuel);
+        
         if (x == 1)
         {
             fuel = fuel - Mathf.Abs(forceX);
@@ -71,7 +88,7 @@ public class main : MonoBehaviour {
         foreach (GameObject planet in planets)
         {
             float force = 0.0f;
-            float distance = getDistance(x, planet.transform.position, this.transform.position);
+            float distance = getDistance( planet.transform.position, this.transform.position);
             float massEquivalent = planet.GetComponent<Collider>().bounds.size.x;
             float forceConstant = 3.0f;
             if (distance > massEquivalent)
@@ -81,6 +98,8 @@ public class main : MonoBehaviour {
             else
             {
                 Destroy(this);
+                button.SetActive(true);
+
             } 
             if(x == 0)
             {
@@ -94,10 +113,12 @@ public class main : MonoBehaviour {
         }
         return finalForce;
     }
-    public float getDistance(int option, Vector3 x, Vector3 y)
+    public float getDistance(Vector3 x, Vector3 y)
     {
         float a = (x.x - y.x)*(x.x - y.x);
         float c = (x.z - y.z)*(x.z - y.z);
         return (a + c);
     }
+
+   
 }
---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class restart : MonoBehaviour {
+    public Button yourButton;
+    
+    void Start () {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(restartLevel);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void restartLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log("Active scene is '" + scene.name + "'.");
+        SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
+        print("Something");
+    }
+}
